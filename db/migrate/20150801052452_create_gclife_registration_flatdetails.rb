b5d63d270dabb4f1becc6ed798b8631ff76514a3
sankardheksitulu <sankar@motifworks.com>
2015-08-09 13:50:29 +0530

    changes

--- db/migrate/20150801052452_create_gclife_registration_flatdetails.rb
+++ db/migrate/20150801052452_create_gclife_registration_flatdetails.rb
@@ -3,11 +3,11 @@ class CreateGclifeRegistrationFlatdetails < ActiveRecord::Migration
     create_table :gclife_registration_flatdetails do |t|
       t.integer :user_id
       t.integer :gclife_registration_id
-      t.integer :societyid
-      t.integer :buildingid
-      t.integer :ownertypeid
-      t.integer :membertypeid
-      t.integer :relationshipid
+      t.string :societyid
+      t.string :buildingid
+      t.string :ownertypeid
+      t.string :membertypeid
+      t.string :relationshipid
       t.date :tenurestart
       t.date :tenureend
 
