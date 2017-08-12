2599b57425376af0df61770e6900547696310ec8
subbiah <subbiahmca@gmail.com>
2017-08-12 13:44:17 +0530

    fix reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,8 @@ config.action_mailer.perform_deliveries = true
 
 config.action_mailer.asset_host = 'http://35.166.172.142:3000'
 
+config.action_mailer.default_url_options = { :host => 'http://35.166.172.142:3000' }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
