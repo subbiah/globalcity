f41ac1a05c79c091c6f20aa95d2e33deee4baa41
subbiah <subbiahmca@gmail.com>
2017-08-12 19:47:29 +0530

    fix reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,25 +33,25 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  
+  config.action_mailer.asset_host = 'http://35.166.172.142:3000'
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => 'gclifetest@gmail.com',
- :password             => 'gclife63',
- :authentication       => "plain",
-:enable_starttls_auto => true
-}
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_deliveries = true
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => 'gclifetest@gmail.com',
+   :password             => 'gclife63',
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
-config.action_mailer.asset_host = 'http://35.166.172.142:3000'
 
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
- # Rails.application.routes.default_url_options[:host] = '35.166.172.142:3000'
+  # Rails.application.routes.default_url_options[:host] = '35.166.172.142:3000'
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
